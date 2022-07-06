8761d9e08005cacabf2b2cfc666db51e7c20a2ca
yjhp1016 <yjhp1016@gmail.com>
2022-07-06 15:00:27 +0100

    add geometry for cavity file

---
+++ Single_phase/flow_domain_geo_generation_2D.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+
+
+# INPUT STL FILE NAME
+output_name = 'geo_cavity.dat'
+
+
+dnx, dny, dnz = 120, 120, 120
+
+#==========================================================
+#           DO NOT CHANGE BELOW
+#==========================================================
+
+out_dat = np.zeros((dnx,dny,dnz))
+
+#=======Can define some geometry here to out_dat=========
+out_dat[0,:,:] = 1
+#cout_dat[:,:,0] = 1
+out_dat[:,0,:] = 1
+out_dat[:,-1,:] = 1
+out_dat[:,:,0] = 1
+out_dat[:,:,-1] = 1
+
+#=========================================================
+
+out_dat = out_dat.reshape(out_dat.size, order = 'F')
+
+
+np.savetxt(output_name,out_dat.T,fmt='%d')
